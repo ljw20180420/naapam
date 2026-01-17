406dffde5970597b55c80141520467f393ae81dc
ljw <ljw2017@sjtu.edu.cn>
2026-01-17 12:41:00 +0800

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from src import align
-
-align.extract_sgRNA_barcode_target(control_dir="/home/ljw/sdb1/naapam/control")
-# align.stat_control_step3(control_dir="/home/ljw/sdb1/naapam/control")
