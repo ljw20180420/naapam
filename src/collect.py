d8ce7764c2c8cebcfdd72822e7850a6f4d792ba6
ljw <ljw2017@sjtu.edu.cn>
2026-01-20 10:39:55 +0800

    Improve collect_data in src/collect.py.

--- src/collect.py
+++ src/collect.py
@@ -7,74 +7,80 @@ import pandas as pd
 from . import utils
 
 
+def read_alg(alg_file: os.PathLike):
+    with subprocess.Popen(
+        args=["sed", "-e", r"N;N;s/\n/\t/g", os.fspath(alg_file)],
+        stdout=subprocess.PIPE,
+    ) as process:
+        df_alg = pd.read_csv(
+            process.stdout,
+            sep="\t",
+            names=[
+                "index",
+                "count",
+                "score",
+                "ref_id",
+                "updangle",
+                "ref_start1",
+                "query_start1",
+                "ref_end1",
+                "query_end1",
+                "random_insertion",
+                "ref_start2",
+                "query_start2",
+                "ref_end2",
+                "query_end2",
+                "downdangle",
+                "cut1",
+                "cut2",
+            ],
+            keep_default_na=False,
+        )
+
+    return df_alg
+
+
 def score(root_dir: os.PathLike):
     root_dir = pathlib.Path(os.fspath(root_dir))
     save_dir = pathlib.Path("figures/hists")
-    df_treats = []
-    for file in os.listdir(root_dir / "align"):
-        with subprocess.Popen(
-            args=["sed", "-e", r"N;N;s/\n/\t/g", root_dir / "align" / file],
-            stdout=subprocess.PIPE,
-        ) as process:
-            df_treats.append(
-                pd.read_csv(
-                    process.stdout,
-                    sep="\t",
-                    names=[
-                        "index",
-                        "count",
-                        "score",
-                        "ref_id",
-                        "updangle",
-                        "ref_start1",
-                        "query_start1",
-                        "ref_end1",
-                        "query_end1",
-                        "random_insertion",
-                        "ref_start2",
-                        "query_start2",
-                        "ref_end2",
-                        "query_end2",
-                        "downdangle",
-                        "cut1",
-                        "cut2",
-                    ],
-                )
-                .groupby("score")["count"]
-                .sum()
-                .reset_index()
-            )
+    df_algs = []
+    for alg_file in os.listdir(root_dir / "align"):
+        df_algs.append(
+            read_alg(root_dir / "align" / alg_file)
+            .groupby("score")["count"]
+            .sum()
+            .reset_index()
+        )
 
-    df_treat: pd.DataFrame = pd.concat(df_treats)
-    df_treat["score"].plot.hist(
-        bins=300, weights=df_treat["count"]
-    ).get_figure().savefig(save_dir / "score.pdf")
+    df_alg = pd.concat(df_algs)
+    df_alg["score"].plot.hist(bins=300, weights=df_alg["count"]).get_figure().savefig(
+        save_dir / "score.pdf"
+    )
 
 
 def collect_data(
-    tables: list[os.PathLike],
-    cut1: int,
-    cut2: int,
+    root_dir: os.PathLike,
     min_score: int,
 ) -> pd.DataFrame:
     """
-    Only collect data. Do not apply any annotation. Only apply read-wise filter.
+    Only collect data. Do not apply any annotation. Only apply read-wise filter such as score.
     """
-
-    # Collect data
-    dfs = []
-    for table in tables:
-        df = pd.read_csv(table, sep="\t", header=0, keep_default_na=False)
-        if len(df) == 0:
+    root_dir = pathlib.Path(os.fspath(root_dir))
+    df_algs = []
+    for alg_file in root_dir / "align":
+        df_alg = read_alg(root_dir / "align" / alg_file)
+        if df_alg.shape[0] == 0:
             continue
-        df = (
-            df.query("score >= @min_score")
+        df_alg = (
+            df_alg.query("score >= @min_score")
             .groupby(
                 [
-                    "barcode",
+                    "ref_id",
                     "ref_end1",
                     "random_insertion",
                     "ref_start2",
+                    "cut1",
+                    "cut2",
                 ]
             )["count"]
             .sum()
@@ -83,25 +89,14 @@ def collect_data(
                 {
                     "ref_end1": "int8",
                     "ref_start2": "int8",
+                    "cut1": "int8",
+                    "cut2": "int8",
                 }
             )
         )
 
-        df = df.assign(
-            cas=utils.infer_cas(table),
-            polq=utils.infer_polq(table),
-            chip=utils.infer_chip(table) + "-" + utils.infer_time(table),
-            sample=utils.infer_sample(table),
-        )
+        df_alg = df_alg.assign(stem=pathlib.Path(alg_file).stem)
 
-        dfs.append(df)
+        df_algs.append(df_alg)
 
-    df = pd.concat(dfs).reset_index(drop=True)
-
-    return df
-
-
-def read_feather(file: os.PathLike):
-    return pd.read_feather(file).assign(
-        random_insertion=lambda df: df["random_insertion"].fillna("")
-    )
+    return pd.concat(df_algs).reset_index(drop=True)
