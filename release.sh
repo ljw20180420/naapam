1701ad3804d6b7bd90c3f5a1021e99b09de76d10
ljw <ljw2017@sjtu.edu.cn>
2026-01-29 17:41:03 +0800

    release v0.1.7

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 uv version --frozen --bump patch
 version="v$(uv version | cut -d" " -f2)"
-git add pyproject.toml
-git commit -m "release ${version}"
+git commit -am "release ${version}"
+git push
 gh release create "${version}" --notes "release ${version}"
 git pull
