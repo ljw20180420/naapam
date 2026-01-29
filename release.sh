9038e7c47cf0ced107e2243d88f5629c0df16eaf
ljw <ljw2017@sjtu.edu.cn>
2026-01-29 17:37:01 +0800

    Update pyproject.toml when release.

--- release.sh
+++ release.sh
@@ -2,5 +2,7 @@
 
 uv version --frozen --bump patch
 version="v$(uv version | cut -d" " -f2)"
+git add pyproject.toml
+git commit -m "release ${version}"
 gh release create "${version}" --notes "release ${version}"
 git pull
