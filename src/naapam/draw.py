45e0f53083e5644c87eb09b151c4d1805c8eaeeb
ljw <ljw2017@sjtu.edu.cn>
2026-02-04 11:03:11 +0800

    draw.py now draw for all stems.

--- src/naapam/draw.py
+++ src/naapam/draw.py
@@ -36,9 +36,7 @@ grid_colors = [
 ]
 
 
-def mean_freq_over_up_del_size_on_tem(
-    root_dir: os.PathLike,
-):
+def mean_freq_over_up_del_size_on_tem(root_dir: os.PathLike):
     root_dir = pathlib.Path(os.fspath(root_dir))
     df_treat = pd.read_csv(
         root_dir / "analyze" / "treat" / "correct" / "treat.csv",
@@ -46,55 +44,76 @@ def mean_freq_over_up_del_size_on_tem(
         na_values=["NA"],
         keep_default_na=False,
     )
-    save_dir = root_dir / "figures" / "analyze" / "mean_freq_over_up_del_size_on_tem"
-
     for tem in range(1, 5):
-        os.makedirs(save_dir / str(tem), exist_ok=True)
-
-        df_tem = (
-            df_treat.query(
-                """
-                    cut2 - ref_start2 == @tem and \
-                    ref_end1 <= cut1
-                """
-            )
-            .reset_index(drop=True)
-            .assign(
-                up_del_size=lambda df: df["cut1"] - df["ref_end1"],
-            )
+        save_dir = (
+            root_dir
+            / "figures"
+            / "analyze"
+            / "mean_freq_over_up_del_size_on_tem"
+            / str(tem)
         )
-
-        df_value = (
-            utils.pivot_value(df=df_tem, value="freq_kim", column="up_del_size")
-            .assign(cas=lambda df: df["stem"].map(utils.infer_cas))
-            .drop(columns=["stem", "ref_id"])
-            .groupby("cas")
-            .sum()
+        os.makedirs(save_dir, exist_ok=True)
+        mean_freq_over_up_del_size_on_tem_inner(
+            df_treat,
+            tem,
+            save_dir / "all.png",
         )
-        df_legal = (
-            utils.pivot_legal(df=df_tem, column="up_del_size")
-            .assign(cas=lambda df: df["stem"].map(utils.infer_cas))
-            .drop(columns=["stem", "ref_id"])
-            .groupby("cas")
-            .sum()
-        )
-        value_vars = df_legal.columns
-
-        df_mean = (
-            (df_value / df_legal)
-            .reset_index()
-            .melt(
-                id_vars="cas",
-                value_vars=value_vars,
-                var_name="up_del_size",
-                value_name="freq_kim",
+        for stem in df_treat["stem"].unique().tolist():
+            df_treat_stem = df_treat.query("stem == @stem").reset_index(drop=True)
+            mean_freq_over_up_del_size_on_tem_inner(
+                df_treat_stem,
+                tem,
+                save_dir / f"{stem}.png",
             )
-        )
 
-        (
-            ggplot(
-                df_mean, aes(x="up_del_size", y="freq_kim", group="cas", color="cas")
-            )
-            + geom_line()
-            + scale_color_manual(values=grid_colors)
-        ).save(save_dir / str(tem) / "freq_kim.pdf")
+
+def mean_freq_over_up_del_size_on_tem_inner(
+    df_treat: pd.DataFrame, tem: int, save_file: os.PathLike
+):
+    df_tem = (
+        df_treat.query(
+            """
+                cut2 - ref_start2 == @tem and \
+                ref_end1 <= cut1
+            """
+        )
+        .reset_index(drop=True)
+        .assign(
+            up_del_size=lambda df: df["cut1"] - df["ref_end1"],
+        )
+    )
+    if not df_tem["legal"].any():
+        return
+
+    df_value = (
+        utils.pivot_value(df=df_tem, value="freq_kim", column="up_del_size")
+        .assign(cas=lambda df: df["stem"].map(utils.infer_cas))
+        .drop(columns=["stem", "ref_id"])
+        .groupby("cas")
+        .sum()
+    )
+    df_legal = (
+        utils.pivot_legal(df=df_tem, column="up_del_size")
+        .assign(cas=lambda df: df["stem"].map(utils.infer_cas))
+        .drop(columns=["stem", "ref_id"])
+        .groupby("cas")
+        .sum()
+    )
+    value_vars = df_legal.columns
+
+    df_mean = (
+        (df_value / df_legal)
+        .reset_index()
+        .melt(
+            id_vars="cas",
+            value_vars=value_vars,
+            var_name="up_del_size",
+            value_name="freq_kim",
+        )
+    )
+
+    (
+        ggplot(df_mean, aes(x="up_del_size", y="freq_kim", group="cas", color="cas"))
+        + geom_line()
+        + scale_color_manual(values=grid_colors)
+    ).save(save_file)
