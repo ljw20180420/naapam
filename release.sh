bbc729d55a5fa88a8a820cc8e20520b1b385f0e6
ljw <ljw2017@sjtu.edu.cn>
2026-05-14 09:59:30 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -4,5 +4,7 @@ uv version --bump patch
 version="v$(uv version | cut -d" " -f2)"
 git commit -am "release ${version}"
 git push
-gh release create "${version}" --notes "release ${version}"
-git pull
+gh release create "${version}" \
+    --generate-notes \
+    --fail-on-no-commits
+git fetch --tags origin
